74e7134575a0c6ff6ca0aad3068f9c4f4eb18eed
xiaotian <istianlei@qq.com>
2023-03-30 22:38:41 +0800

    feat: Landmarks views

--- aqiapp/Views/ContentView.swift
+++ aqiapp/Views/ContentView.swift
@@ -10,43 +10,44 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
+    
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
-
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
+        LandmarkList()
+        //        NavigationView {
+        //            List {
+        //                ForEach(items) { item in
+        //                    NavigationLink {
+        //                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+        //                    } label: {
+        //                        Text(item.timestamp!, formatter: itemFormatter)
+        //                    }
+        //                }
+        //                .onDelete(perform: deleteItems)
+        //            }
+        //            .toolbar {
+        //                ToolbarItem(placement: .navigationBarTrailing) {
+        //                    EditButton()
+        //                }
+        //                ToolbarItem {
+        //                    Button(action: addItem) {
+        //                        Label("Add Item", systemImage: "plus")
+        //                    }
+        //                }
+        //            }
+        //            Text("Select an item")
+        //        }
     }
-
+    
     private func addItem() {
         withAnimation {
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date()
-
+            
             do {
                 try viewContext.save()
             } catch {
@@ -57,11 +58,11 @@ struct ContentView: View {
             }
         }
     }
-
+    
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
             offsets.map { items[$0] }.forEach(viewContext.delete)
-
+            
             do {
                 try viewContext.save()
             } catch {
---
+++ aqiapp/Views/LandmarkList.swift
@@ -0,0 +1,35 @@
+//
+//  LandmarkList.swift
+//  aqiapp
+//
+//  Created by xiaotian on 2023/3/30.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+                
+            }
+            .navigationTitle("Landmarks")
+            
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone 14 Pro"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
