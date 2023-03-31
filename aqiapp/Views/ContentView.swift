b889a0c33b7311ba6b800b7fbf0cdf415dc971f9
xiaotian <istianlei@qq.com>
2023-03-31 19:13:36 +0800

    feat: input

--- aqiapp/Views/ContentView.swift
+++ aqiapp/Views/ContentView.swift
@@ -85,5 +85,7 @@ private let itemFormatter: DateFormatter = {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+            .environmentObject(ModelData())
+
     }
 }
--- aqiapp/aqiappApp.swift
+++ aqiapp/aqiappApp.swift
@@ -10,11 +10,13 @@ import SwiftUI
 @main
 struct aqiappApp: App {
     let persistenceController = PersistenceController.shared
+    @StateObject private var modelData = ModelData()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(modelData)
         }
     }
 }
