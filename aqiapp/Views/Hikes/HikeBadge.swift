73e3988694f269dc2d251a334875f9add706425f
xiaotian <istianlei@qq.com>
2023-03-31 23:28:30 +0800

    feat: Working with UI Controls

---
+++ aqiapp/Views/Hikes/HikeBadge.swift
@@ -0,0 +1,30 @@
+//
+//  HikeBadge.swift
+//  aqiapp
+//
+//  Created by xiaotian on 2023/3/31.
+//
+
+import SwiftUI
+
+struct HikeBadge: View {
+    var name: String
+
+    var body: some View {
+        VStack(alignment: .center) {
+            Badge()
+                .frame(width: 300, height: 300)
+                .scaleEffect(1.0 / 3.0)
+                .frame(width: 100, height: 100)
+            Text(name)
+                .font(.caption)
+                .accessibilityLabel("Badge for \(name).")
+        }
+    }
+}
+
+struct HikeBadge_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeBadge(name: "Preview Testing")
+    }
+}
